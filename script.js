9c38f2a335910a256460e8f7e5ed9ded2f4601c0
Leonardo <leo.braga@eaportal.org>
2025-10-23 12:13:24 -0300

    Criando uma variável button e adicionando o evento do botão

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const button = document.querySelector("button")
+button.addEventListener("click", receiveData)
+
 function receiveData() {
 
     fetch("http://localhost:3333").then(response => response.json()).then(data => console.log(data.message))
