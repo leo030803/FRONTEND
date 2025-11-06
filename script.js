808162aad7d2ad207700124d1796c0f93626a3b5
Leonardo <leo.braga@eaportal.org>
2025-11-05 21:55:02 -0300

    Colocando a section e a async function

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const button = document.querySelector("button")
 button.addEventListener("click", receiveData)
 
-function receiveData() {
+ async function receiveData() {
 
-    fetch("http://localhost:3333").then(response => response.json()).then(data => console.log(data.message))
+   const persons = await fetch("http://localhost:3333").then(response => response.json())
+
+   persons.map(person => {
+
+     document.querySelector("main").innerHTML += `
+     
+     <section>
+     
+     <h2>Nome: ${person.name}</h2>
+     <p>Email: ${person.email}</p>
+     <p>Idade: ${person.age}</p>
+     <p>Apelido: ${person.nickname}</p>
+
+
+
+    </section>
+     
+     
+     `
+
+   })
 }
