fdc0e39526201e9152d3192315fadeb0e7ffcc9b
Leonardo <leo.braga@eaportal.org>
2025-11-05 22:56:02 -0300

    Criação do cadastro.js e a variável user para os dados do cadastro

---
+++ pages/cadastro.js
@@ -0,0 +1,34 @@
+const button = document.querySelector("button")
+button.onclick = (event) => {
+    
+    event.preventDefault()
+   
+    signUpEagle()
+}
+
+
+function signUpEagle() {
+
+     const name = document.querySelector("#name").value
+     const email = document.querySelector("#email").value
+     const age = document.querySelector("#age").value
+     const nickname = document.querySelector("#nickname").value
+
+     if (name === "" || email === "" || age === "" || nickname === "") {
+
+        alert("Preencha todas as informações!")
+        return
+     }
+
+     const user = {
+
+        name,
+        email,
+        age,
+        nickname
+     }
+
+     console.log(user)
+
+     // enviar o user para o backend
+}
