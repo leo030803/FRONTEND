c67a8281ed5f437f2be7b180d1cf9b10250d220c
Leonardo <leo.braga@eaportal.org>
2025-12-04 10:11:00 -0300

    Criação do login

---
+++ pages/login/login.js
@@ -0,0 +1,54 @@
+const button = document.querySelector("button")
+button.onclick = (event) => {
+    
+    event.preventDefault()
+   
+    signUpEagle()
+}
+
+
+ async function signUpEagle() {
+
+     const name = document.querySelector("#name").value
+     const email = document.querySelector("#email").value
+     const age = document.querySelector("#age").value
+     const nickname = document.querySelector("#nickname").value
+     const password = document.querySelector("#password").value
+
+     if (name === "" || email === "" || age === "" || nickname === "") {
+
+        alert("Preencha todas as informações!")
+        return
+     }
+
+     const user = {
+
+        name,
+        email,
+        age,
+        nickname,
+        password
+     }
+
+     console.log(user)
+
+     // enviar o user para o backend
+
+     const response = await fetch("http://localhost:3333/cadastrar", {
+
+      method: "POST",
+      headers: {
+
+         "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({ user })
+
+ } ).then(response => response.json())
+
+   const { message } = response
+
+   alert(message)
+
+   window.location.href = "../index.html"
+}
