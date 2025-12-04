8849ba9cfbff72048a5b43ca9fdbb59a4f9572dd
Leonardo <leo.braga@eaportal.org>
2025-12-04 08:00:59 -0300

    Criação do arquivo usuários.html e js

--- usuarios.js
+++ usuarios.js
@@ -1,7 +1,4 @@
-const button = document.querySelector("button")
-button.addEventListener("click", receiveData)
-
- async function receiveData() {
+async function receiveData() {
 
    const persons = await fetch("http://localhost:3333").then(response => response.json())
 
